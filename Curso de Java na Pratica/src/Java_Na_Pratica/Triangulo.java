9a2759855ec0382d119afb9bb4f78360dc77b6bc
Cristian <cristian.ataliba@outlook.com>
2021-05-19 00:26:12 -0300

    Atualiza Java na prática

---
+++ Curso de Java na Pratica/src/Java_Na_Pratica/Triangulo.java
@@ -0,0 +1,36 @@
+package Java_Na_Pratica;
+
+import java.util.Scanner;
+//import java.util.*  "isso importa todas as classes do pacote java.util"
+
+public class Triangulo {
+	
+	//Realize um programa que calcule a hipotenusa de um triângulo retângulo a partir dos catetos
+	//h² = a²+ b²
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		
+		double hipotenusa;
+		
+		System.out.println("Informe o valor do cateto1: ");
+		float cateto1 = in.nextFloat();
+		
+		System.out.println("Inform o valor do cateto2: ");
+		float cateto2 = in.nextFloat();
+		
+		
+		
+		hipotenusa = Math.sqrt(Math.pow(cateto1,2)+ (Math.pow(cateto2,2)));
+		//A Raíz quadrada Math.sqrt
+		//Math.pow eleva um numero a outro
+		
+		System.out.println("A Hipotenusa é: "+Math.round(hipotenusa));
+		//Math.round arredonda o reultado caso dê algum valor quebrado.
+		
+	
+
+	}
+
+}

410a764cac2b6efddb190d393a6061b71cd8d4d7
Cristian <cristian.ataliba@outlook.com>
2021-05-28 16:24:31 -0300

    Atualiza curso Java na Prática - Códigos

---
+++ Curso de Java na Pratica/src/Java_Na_Pratica/Array1.java
@@ -0,0 +1,84 @@
+package Java_Na_Pratica;
+import java.util.*;
+import java.util.Arrays;
+
+//Realize um código que tenha um array de 10 elementos do tipo inteiro e crie definções para:
+//Exibir somente números para e exibir quantos são esse números
+//Exibir o Array ordenamente 
+//Realizar a pesquisa de um indeice de um elemento
+//Ultilizar o calsse array
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		
+		int vetor[] = new int [10];// Cria um array de 9 posições
+		Scanner in = new Scanner(System.in); // Cria um objeto da classe Scanner para ler dados de entrada do teclado.
+
+//########################################################################################################################		
+		
+		//Declaração de váriáveis
+		int i, contador=0; //Cria uma váriável chamada i e inicia o contador.
+		
+		for(i = 0; i < 10; i++ ) {
+			System.out.println("Elemento "+i+":");
+			vetor [i] = in.nextInt(); // Armazena o número digitado pelo usúario no array até 9 posições
+			
+			System.out.println(); // Pula uma linha
+		}//Fim do laço for	
+//#############################################################################################################			
+			//Exibir números pares
+			System.out.println("Exibindo números pares");
+			for(i = 0; i<10; i++) {
+				
+				if(vetor[i] % 2 == 0) {// Se o vetor armazenado for dívisível por 2 ou seja par e ter resto 0
+					System.out.print(vetor[i]+ " ");// Quando não tem o ln(do print) exibe os elementos na horizontal separado
+					//por um espeço em branco
+					contador++; //incrementa somente com números pares
+				}//Fim do laço for	
+		}
+	
+			
+			
+//################################################################################################################
+			
+			System.out.println(); // Pula uma linha
+			
+//################################################################################################################			
+			
+		
+		
+		
+		
+		System.out.println("Quantidade de números pares "+contador);//Exibe a quantidade de números pares
+		//contados a partir de um contador na condicional if
+		
+//################################################################################################################
+		
+		System.out.println();//Pula uma linha
+		
+//################################################################################################################
+		
+		Arrays.sort(vetor);//Usando o método sort da classe Arrays para ordenar o Array vetor
+		
+		
+		System.out.println("Exibindo oredenadamente");
+		for(i = 0; i<10; i++) {
+			
+			System.out.println(vetor[i]);
+		}
+		
+//###############################################################################################################		
+		
+		System.out.println("Informe o elemento a ser pesquisado: ");
+		int elemento = in.nextInt();
+		
+		System.out.println("Indice do elemento "+Arrays.binarySearch(vetor, elemento));
+		
+		//binary search exibe o índice de um elemento informado
+		
+//################################################################################################################		
+
+	}
+
+}

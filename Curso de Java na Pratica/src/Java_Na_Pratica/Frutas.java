e166fae2381db9613edec82c918b198a3bd6dacb
Cristian <cristian.ataliba@outlook.com>
2021-06-26 18:00:34 -0300

    Atualiza curso Java

--- Curso de Java na Pratica/src/Java_Na_Pratica/Frutas.java
+++ Curso de Java na Pratica/src/Java_Na_Pratica/Frutas.java
@@ -5,12 +5,26 @@ public class Frutas {
 
 	public static void main(String[] args) {
 		
-		String frutas []= {"Abacate", "Abacaxi"};
+		String cidades []= {"Matão", "São Carlos", "Araraquara"};
+		String nomes[] = {"João", "Manoel", "Carlos"};
+		String telefones [] = {"3339-4444", "3339-4443", "3339-4442"};
 		
 		
 		
-		System.out.println("Exibindo total de frutas: "+frutas.length);
-		System.out.println(frutas[0]);
+		System.out.println("Exibindo total de cidades no array: "+cidades.length);
+		System.out.println("Exibindo primeiro contato na cidade: "+ cidades[0]);
+		
+		System.out.println();
+		
+		System.out.println("Exibindo total de nome no array: "+ nomes.length);
+		System.out.println("Exibindo segundo nome no array: " + nomes[1]);
+		
+		System.out.println();
+		
+		System.out.println("Exibindo o total de telefones no array: "+ telefones.length);
+		System.out.println("Exibindo terceiro telefone no array: " + telefones[2]);
+		
+		
 		
 		
 		

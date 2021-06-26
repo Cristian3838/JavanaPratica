3d4c0b28ff172dfed220b2dfc6d026808bfe1862
Cristian <cristian.ataliba@outlook.com>
2021-06-26 17:06:09 -0300

    Atualiza curso Java

---
+++ Curso de Java na Pratica/src/Java_Na_Pratica/Menu.java
@@ -0,0 +1,47 @@
+package Java_Na_Pratica;
+import java.util.*;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		
+		int menu = 0;
+		
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Escolha o menu de opções abaixo!");
+		System.out.println("1 - NetFlix, 2 - HBO, 3 - Amazon Prime, 4 - Sair do sistema.");
+		System.out.println("Qual sua escolha: ");
+		menu = in.nextInt();
+		
+		
+		switch(menu) {
+		
+		case 1:
+			System.out.println("Abrindo programação NetFlix!");
+			break;
+			
+		case 2:
+			System.out.println("Abrindo programação HBO!");
+			break;
+		
+		case 3:
+			System.out.println("Abrindo Amazon Prime!");
+			break;
+			
+		case 4:
+			System.out.println("Sair do sistema!");
+			break;
+			
+			default:
+				System.out.println("Você deve escolher as opções 1,2 ou 3!");
+				break;
+		
+		
+		}
+		
+	}
+
+}

6460dc41bec2ed4c733390ec2bee05dce4f1617f
Cristian <cristian.ataliba@outlook.com>
2021-06-26 17:50:33 -0300

    Atualiza curso Java

---
+++ Curso de Java na Pratica/src/Java_Na_Pratica/Frutas.java
@@ -0,0 +1,19 @@
+package Java_Na_Pratica;
+import java.util.*;
+
+public class Frutas {
+
+	public static void main(String[] args) {
+		
+		String frutas []= {"Abacate", "Abacaxi"};
+		
+		
+		
+		System.out.println("Exibindo total de frutas: "+frutas.length);
+		System.out.println(frutas[0]);
+		
+		
+		
+	}
+
+}
